22631b89f329e55751dff8943eeb5e9c76f83961
Fleetwood <wizening@gmail.com>
2020-01-26 15:25:13 -0600

    Fleetwood (#5)
    * Fleetwood init
    
    * denney potential fuckery
    
    * Adding guid methods
    
    * lets try this again
    
    * Added PrimaryObject guid
    
    * Meetups small change
    
    * Delete extra arg from constructor on Meetup
    
    * Cleaned up imports to require
    
    * Updated Primary Object ctor
    
    * Add remaining getters and setters
    
    * Corrected module.exports.
    
    * Correcting exports.
    
    * Corrected some of the class ctors. Initialized referenced objects.
    
    * Rev merge dev.
    
    * Adding launch.json
    
    * Adding gitingore
    
    * Adding Models
    
    Co-authored-by: Fleetwood-Peloton <54635568+Fleetwood-Peloton@users.noreply.github.com>
    Co-authored-by: JLDenney <52334078+JLDenney@users.noreply.github.com>

---
+++ Models.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Instructor: require('./models/Instructor'),
+    Meetup: require('./models/Meetup'),
+    Student: require('./models/Student'),
+    Timeslot: require('./models/Timeslot'),
+};
