ec63515fae486384163510489d18789cad2bbd7d
Fleetwood <john.fleetwood@onepeloton.com>
2020-02-02 17:01:10 -0500

    Resolving conflicts.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-    ExtendedArray: require('./ExtendedArray'),
-=======
     Calendar: require('./Calendar'),
->>>>>>> dev
     Instructor: require('./Instructor'),
     Meetup: require('./Meetup'),
     Student: require('./Student'),
