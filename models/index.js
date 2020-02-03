ef4d6b60062cd64d31172115a289223951012c3f
Johnathon Denney <jld.author@gmail.com>
2020-02-03 09:24:54 -0500

    Deleted trailing merge conflict markup in Index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,9 @@
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
     Calendar: require('./Calendar'),
-=======
-<<<<<<< HEAD
+
     ExtendedArray: require('./ExtendedArray'),
-=======
+
     Calendar: require('./Calendar'),
->>>>>>> dev
->>>>>>> db65418923de04b269bddd8b06b278cc7cfa4b32
-=======
->>>>>>> 8ba0f225c15803da93036dcc5ee9847a00b8b9ab
     Instructor: require('./Instructor'),
     Meetup: require('./Meetup'),
     Student: require('./Student'),
