846474ca51e3cba6d6c4dc125c6976f991855d04
Fleetwood <john.fleetwood@onepeloton.com>
2020-02-22 15:59:21 -0500

    Handlebars and routes in place.

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const browserSync = require('browser-sync');
 const express = require('express');
-// const shrinkRay = require('shrink-ray');
-
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
+const hbs = require('express-hbs');
+app.use(function (req, res, next) {
+    req.rawBody = '';
+    req.on('data', (chunk) => req.rawBody += chunk);
+
+    next();
+});
+
+// app.use(favicon(path.join(__dirname, './', 'favicon.ico')))
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// set up handlebars ready for tabs
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views/');
+app.use(express.static('public/'));
 const port = 9777; // "xprs" in T9
 
-// you can conditionally add routes and behaviour based on environment
-const isProduction = 'production' === process.env.NODE_ENV;
-
-app.set('etag', isProduction);
 app.use((req, res, next) => { res.removeHeader('X-Powered-By'); next(); });
-// app.use(shrinkRay());
-
-// static example, add real routes here instead
-app.use('/', express.static('src/', { etag: isProduction, lastModified: false }));
-
+app.get('/', (req, res) => {
+    res.render('index', {
+      domain: req.get('host'),
+      protocol: req.protocol,
+      title: 'Homecoming',
+      layout: 'layouts/default'
+    });
+  })
 app.listen(port, listening);
 
 function listening () {
     console.log(`Server available on http://localhost:${port}`);
-    if(!isProduction) {
-        browserSync({
-            files: ['src/**/*.{html,js,css}'],
-            online: true,
-            open: true,
-            port: port + 1,
-            proxy: 'localhost:' + port,
-            ui: false
-        });
-    }
+    browserSync({
+        files: ['public/**/*.{html,js,css}', 'views/**/*.{html,hbs}'],
+        online: true,
+        open: true,
+        port: port + 1,
+        proxy: 'localhost:' + port,
+        ui: false
+    });
 }
