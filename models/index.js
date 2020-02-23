bf788fbf676424c37c631b65771f04ecac731f9d
Johnathon Denney <jld.author@gmail.com>
2020-02-23 17:13:21 -0500

    Converted requires in Index to plural form

--- models/index.js
+++ models/index.js
@@ -1,23 +1,23 @@
 const knex = require('./knex')
     , Users = require('./Users')
-    , ClassTypePreference = require('./ClassTypePreference')
-    , ClassTimeSlotPreference = require('./ClassTimeSlotPreference')
-    , Person = require('./Person')
-    , Session = require('./Session')
-    , UserAttenancePreference = require('./UserAttendancePreference')
-    , UserClassTimeSlotPreference = require('./UserClassTimeSlotPreference')
-    , UserClassTypePreference = require('./UserClassTypePreference')
-    , UserInstructorPreference = require('./UserInstructorPreference')
+    , ClassTypePreferences = require('./ClassTypePreference')
+    , ClassTimeSlotPreferences = require('./ClassTimeSlotPreference')
+    , Persons = require('./Person')
+    , Sessions = require('./Session')
+    , UserAttenancePreferences = require('./UserAttendancePreference')
+    , UserClassTimeSlotPreferences = require('./UserClassTimeSlotPreference')
+    , UserClassTypePreferences = require('./UserClassTypePreference')
+    , UserInstructorPreferences = require('./UserInstructorPreference')
 
 module.exports = {
     db: knex.db,
     Users,
-    ClassTypePreference,
-    ClassTimeSlotPreference,
-    Person,
-    Session,
-    UserAttenancePreference,
-    UserClassTimeSlotPreference,
-    UserClassTypePreference,
-    UserInstructorPreference,
+    ClassTypePreferences,
+    ClassTimeSlotPreferences,
+    Persons,
+    Sessions,
+    UserAttenancePreferences,
+    UserClassTimeSlotPreferences,
+    UserClassTypePreferences,
+    UserInstructorPreferences,
 }
